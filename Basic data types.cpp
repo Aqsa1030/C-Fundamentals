410fc97dd34a67b87eb70b26d9368666a55d4ca4
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-14 10:55:34 +0500

    Update Basic data types.cpp

--- Basic data types.cpp
+++ Basic data types.cpp
@@ -1,18 +1,13 @@
-
 #include <iostream>
 using namespace std;
 int main()
 {
     int a;  long l;    char c;     float b;   double n;
-    cout << "Put Values :\n";
     cin >> a >> l >> c >> b >> n;
     cout << "Integer : " << a << "\n";
     cout << "Long : " << l << "\n";
     cout << "Char : " << c << "\n";
     cout << "Float : " << b << "\n";
     cout << "Double : " << n << "\n";
-
-
-
 }
 
