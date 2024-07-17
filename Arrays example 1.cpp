7c245105ac75c709dfd4973c7dee7574f78d2c0b
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-17 20:03:45 +0500

    Add files via upload

---
+++ Arrays example 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = { 1, 2, 3, 4, 5 };
+    int reverse[5];
+
+    // Reverse the array
+    for (int i = 0; i < 5; i++) {
+        reverse[i] = arr[4 - i];
+    }
+
+    // Output the reversed array
+    cout << "Reversed array: ";
+    for (int i = 0; i < 5; i++) {
+        cout << reverse[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
