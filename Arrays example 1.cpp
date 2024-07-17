5d895c5a092acec0ad2e1cdf983f49eba6e84be5
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-17 20:05:17 +0500

    Update Arrays example 1.cpp

--- Arrays example 1.cpp
+++ Arrays example 1.cpp
@@ -5,17 +5,15 @@ int main() {
     int arr[5] = { 1, 2, 3, 4, 5 };
     int reverse[5];
 
-    // Reverse the array
     for (int i = 0; i < 5; i++) {
         reverse[i] = arr[4 - i];
     }
 
-    // Output the reversed array
+    // Output reversed array
     cout << "Reversed array: ";
     for (int i = 0; i < 5; i++) {
         cout << reverse[i] << " ";
     }
     cout << endl;
-
     return 0;
 }
