9131bfcec0aae5da80fe900ddb1479214655c3b8
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-18 20:18:00 +0500

    Update Arrays example 5.cpp

--- Arrays example 5.cpp
+++ Arrays example 5.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int arr1[5] = { 10, 20, 30, 40, 50 };
     int arr2[5];
-
-    // Copy arr1 to arr2
+    
     for (int i = 0; i < 5; ++i) {
         arr2[i] = arr1[i];
     }
 
-    std::cout << "Array 2: ";
+    cout << "Array 2: ";
     for (int i = 0; i < 5; ++i) {
-        std::cout << arr2[i] << " ";
+        cout << arr2[i] << " ";
     }
-    std::cout << std::endl;
-
+    cout << std::endl;
     return 0;
 }
