523b4b9f94e4f07a7619c39ebb4c34edaf991007
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-18 20:19:32 +0500

    Update Array example 6.cpp

--- Array example 6.cpp
+++ Array example 6.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int arr1[3] = { 1, 2, 3 };
     int arr2[3] = { 4, 5, 6 };
     int Merge[6];
 
-    // Merge arr1 and arr2
     for (int i = 0; i < 3; ++i) {
         Merge[i] = arr1[i];
         Merge[i + 3] = arr2[i];
     }
-
-    std::cout << "Merged Array: ";
+    cout << "Merged Array: ";
     for (int i = 0; i < 6; ++i) {
-        std::cout << Merge[i] << " ";
+        cout << Merge[i] << " ";
     }
-    std::cout << std::endl;
-
+    cout << std::endl;
     return 0;
 }
