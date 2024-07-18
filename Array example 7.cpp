799f669f459974fca18bddf104c745950d48e792
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-18 20:21:33 +0500

    Update Array example 7.cpp

--- Array example 7.cpp
+++ Array example 7.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int arr[8] = { 1, 2, 3, 1, 4 ,8,1 ,7};
     int target = 1;
     int count = 0;
-
     for (int i = 0; i < 8; ++i) {
         if (arr[i] == target) {
             count++;
         }
     }
-
-    std::cout << "Occurrences of " << target << ": " << count << std::endl;
+    cout << "Occurrences of " << target << ": " << count << std::endl;
     return 0;
 }
