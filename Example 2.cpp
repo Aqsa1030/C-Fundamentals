f3a869692b1ba13719c4ee4b3fc9524b6e266375
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-20 20:21:13 +0500

    Add files via upload

---
+++ Example 2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+    char choice;
+    double temperature, convertedTemp;
+
+    cout << "Choose conversion:\n";
+    cout << "1. Fahrenheit to Celsius\n";
+    cout << "2. Celsius to Fahrenheit\n";
+    cout << "Enter your choice (1 or 2): ";
+    cin >> choice;
+
+    switch (choice) {
+    case '1':
+        cout << "Enter temperature in Fahrenheit: ";
+        cin >> temperature;
+        convertedTemp = (temperature - 32) * 5 / 9;
+        cout << "Temperature in Celsius: " << convertedTemp << " °C";
+        break;
+    case '2':
+        cout << "Enter temperature in Celsius: ";
+        cin >> temperature;
+        convertedTemp = (temperature * 9 / 5) + 32;
+        cout << "Temperature in Fahrenheit: " << convertedTemp << " °F";
+        break;
+    default:
+        cout << "Invalid choice!";
+        break;
+    }
+
+    cout << endl;
+
+    return 0;
+}
