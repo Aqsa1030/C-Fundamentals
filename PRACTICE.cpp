a66cfb890cdafe5a2c025fc755c531c12748948a
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-27 18:36:10 +0500

    Update PRACTICE.cpp

--- PRACTICE.cpp
+++ PRACTICE.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main()
 {
     int a, b;
-cout << "Hello World!\n\n";
-cout << "Enter two integers :";
-cin >> a >> b;
-int sum = a + b;
+    cout << "Hello World!\n\n";
+    cout << "Enter two integers :";
+    cin >> a >> b;
+    int sum = a + b;
 int quotient = a / b;
 int diff = a - b;
 int mult = a * b;
