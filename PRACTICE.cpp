7c280642fd37e8704fe8c9a283ed54973d82656b
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-28 18:42:37 +0500

    Update PRACTICE.cpp

--- PRACTICE.cpp
+++ PRACTICE.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int a, b, c;
     cout << "Hello World!\n\n";
-    cout << "Enter two integers :";
+    cout << "Enter two integers : ";
     cin >> a >> b;
     int sum = a + b;
     int quotient = a / b;
@@ -16,7 +16,7 @@ int main()
     cout << "MULTIPLICATION = " << mult << endl;
     cout << "QUOTIENT = " << quotient << endl;
     cout << endl;
-    cout << "Enter a numberto find whether it is even or odd : ";
+    cout << "Enter a number to find whether it is even or odd : ";
     cin >> c;
     return 0;
     
