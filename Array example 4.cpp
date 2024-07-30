82f51b2c1c5614687c0c3a7bf3e0173fea0302f2
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-30 20:02:15 +0500

    Update Array example 4.cpp

--- Array example 4.cpp
+++ Array example 4.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-    // Declaration and initialization
+    
     int arr[10] = { 10, 20, 30, 40, 50,90, 76, 54, 23, 99};
     int sum = 0;
-
-    // Calculate the sum 
+    
     for (int i = 0; i < 10; i++) {
         sum += arr[i];
     }
