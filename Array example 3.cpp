59a527572b55f56fc2a501327f67e23c2d8db3db
Aqsa1030 <aqsaaslam182005@gmail.com>
2024-07-30 20:00:44 +0500

    Update Array example 3.cpp

--- Array example 3.cpp
+++ Array example 3.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-    // Declare and initialize an array
+    
     int arr[8] = { 3, 1, 4, 1, 5, 77, 90, 123 };
     int max = arr[0];
-
-    // Find the maximum element
+    
     for (int i = 1; i < 8  ; i++) {
         if (arr[i] > max) {
             max = arr[i];
